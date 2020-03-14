b723f8022120fab0464db1aabdc3981dea734ab7
Matheus Queiroz <matheusqz@gmail.com>
2020-03-14 08:40:53 -0300

    Add Docker configs

---
+++ docker/script.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+docker stop $(docker ps -a -q)
+docker system prune --force
+docker network create compartilhada
+docker-compose up -d
+sleep 30s
+docker-compose exec resourcemanager apt-get update
+docker-compose exec -d resourcemanager apt-get -y install python
+docker-compose exec nodemanager1 apt-get update
+docker-compose exec -d nodemanager1 apt-get -y install python
+docker-compose exec nodemanager2 apt-get update
+docker-compose exec -d nodemanager2 apt-get -y install python
+docker-compose exec nodemanager3 apt-get update
+docker-compose exec -d nodemanager3 apt-get -y install python
+docker ps -q | xargs docker inspect --format "{{range .NetworkSettings.Networks}}{{print .IPAddress}} {{end}}{{.Name}}"
+echo "Pronto! =)"
